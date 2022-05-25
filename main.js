267c77c11f8f2da790d78b02ffc7b9dcb0450c16
Popov-Alexey <alexey_popov@yahoo.com>
2022-05-25 22:54:42 +0300

    Added first logic of my page

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log(123);
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    document.getElementById("whoareyou").addEventListener("click", getLifeRule);
+
+    function getLifeRule()
+    {
+        let items = [
+            "Хуибола",
+            "Говноед",
+            "Хлебопашец",
+            "Сиськонюх",
+            "Жопоплюх"
+        ], block_text = document.querySelector(".life-rule__text");
+        let random_item = items[Math.floor(Math.random()*items.length)];
+        block_text.textContent = random_item;
+    }
+});
